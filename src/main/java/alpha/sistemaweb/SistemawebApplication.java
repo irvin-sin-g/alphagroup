0253d86eef57e818a2d090121f27078861519488
irvin-sin-g <irvin.newlife@gmail.com>
2026-09-19 11:46:47 -0600

    Moviendo contenido del proyecto a la raiz del repo

---
+++ src/main/java/alpha/sistemaweb/SistemawebApplication.java
@@ -0,0 +1,13 @@
+package alpha.sistemaweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemawebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemawebApplication.class, args);
+	}
+
+}
